28a00ad9cabe63b1619617f1ca6a9936b228de55
Rico Chow <rchow@192-168-0-100.rdsnet.ro>
2018-08-31 21:02:43 +0300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -16,6 +16,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newEmail', {
+    from: 'rico@example.com',
+    text: 'What is good?',
+    createdAt: 123
+  });
+
+  socket.emit('newMessage', {
+    from: 'Rico',
+    text: 'Hello',
+    createdAt: 321
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () =>{
     console.log('User disconnected');
   });
