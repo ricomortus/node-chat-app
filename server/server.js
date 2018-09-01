4a67fd86c7bc4fcc2629b10662e3754357c312e6
Rico Chow <rchow@192-168-0-100.rdsnet.ro>
2018-09-01 16:32:05 +0300

    Add form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -24,16 +24,16 @@ io.on('connection', (socket) => {
     console.log('createEmail', newEmail);
   });
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-
+    callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // });
-
+    
   });
 
   socket.on('disconnect', () =>{
