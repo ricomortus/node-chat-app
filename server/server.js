75b15b975aeb4ef330fd88571be44eb27cba5dae
Rico Chow <rchow@192-168-0-100.rdsnet.ro>
2018-09-01 21:10:51 +0300

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -20,28 +20,20 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
 
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
-  });
-
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server.');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
-    
+    callback('This is from the server.');    
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude,coords.longitude));
   });
 
   socket.on('disconnect', () =>{
     console.log('User disconnected');
   });
 });
-
-
 
 
 server.listen(port, () => console.log(`Server started on port ${port}!`));
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,27 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
-    var from = 'Rico'
+    var from = 'Rico';
     var text = 'Some message';
     var message = generateMessage(from,text);
     
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Rico';
+    var latitude = 44.4880503;
+    var longitude = 26.0841656;
+    var url = 'https://www.google.com/maps?q=44.4880503,26.0841656';
+    var message = generateLocationMessage(from, latitude, longitude);
+    
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
